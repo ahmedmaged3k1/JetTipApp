f0f474f50a17637c1675ce567d1f88b19d269bd7
ahmedmaged3k1 <60134186+ahmedmaged3k1@users.noreply.github.com>
2022-03-09 14:04:21 +0200

    JetTipApp

--- app/src/main/java/com/example/jettipapp/MainActivity.kt
+++ app/src/main/java/com/example/jettipapp/MainActivity.kt
@@ -8,7 +8,10 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Slider
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Remove
@@ -27,6 +30,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jettipapp.components.InputField
 import com.example.jettipapp.ui.theme.JetTipAppTheme
+import com.example.jettipapp.util.calculateTotalPerPerson
+import com.example.jettipapp.util.calculateTotalTip
 import com.example.jettipapp.widgets.RoundIconButton
 
 class MainActivity : ComponentActivity() {
@@ -35,8 +40,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetTipAppTheme {
                 MyApp {
-                    Text(text = "Hello Again")
-                    TopHeader()
+                    MainContent()
 
                 }
             }
@@ -52,42 +56,6 @@ fun MyApp(content: @Composable () -> Unit) {
         color = MaterialTheme.colors.background
     ) {
         content()
-        MainContent()
-
-    }
-
-}
-
-//@Preview
-@Composable
-fun TopHeader(totalPerPerson: Double = 134.0) {
-    androidx.compose.material.Surface(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(150.dp)
-            .clip(shape = RoundedCornerShape(corner = CornerSize(12.dp))),
-        color = Color(0xFFE9D7F7)
-
-    ) {
-        Column(
-            modifier = Modifier.padding(5.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            val total = "%.2f".format(totalPerPerson)
-            Text(
-                text = "Total Per Person",
-                style = MaterialTheme.typography.h5,
-                fontWeight = Bold
-            )
-            Text(
-                text = "$ $totalPerPerson ",
-                style = MaterialTheme.typography.h4,
-                fontWeight = ExtraBold
-
-            )
-
-        }
 
 
     }
@@ -99,8 +67,8 @@ fun TopHeader(totalPerPerson: Double = 134.0) {
 @Preview
 @Composable
 fun MainContent() {
-
-    BillForm() {
+    Column() {
+        BillForm()
 
     }
 
@@ -122,6 +90,20 @@ fun BillForm(
     val sliderPositionState = remember {
         mutableStateOf(0f)
     }
+    val splitByState = remember {
+        mutableStateOf(1)
+    }
+    val tipAmmountState = remember {
+        mutableStateOf(0.0)
+    }
+    val tipPercentage = (sliderPositionState.value * 100).toInt()
+    val range = IntRange(start = 1, endInclusive = 100)
+    val totalPerPersonState = remember {
+        mutableStateOf(0.0)
+    }
+
+
+    TopHeader(totalPerPersonState.value)
 
     Surface(
         modifier = Modifier
@@ -148,82 +130,148 @@ fun BillForm(
                     keyboardController?.hide()
                 }
             )
-            //    if (validState) {
-            Row(
-                modifier = Modifier.padding(3.dp),
-                horizontalArrangement = Arrangement.Start
-            ) {
-                Text(
-                    text = "Split ",
-                    modifier = Modifier.align(alignment = Alignment.CenterVertically)
-                )
-                Spacer(modifier = Modifier.width(120.dp))
+            if (validState) {
+                Row(
+                    modifier = Modifier.padding(3.dp),
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    Text(
+                        text = "Split ",
+                        modifier = Modifier.align(alignment = Alignment.CenterVertically)
+                    )
+                    Spacer(modifier = Modifier.width(120.dp))
+                    Row(
+                        modifier = Modifier.padding(horizontal = 3.dp),
+                        horizontalArrangement = Arrangement.End
+                    ) {
+                        RoundIconButton(imageVector = Icons.Default.Remove,
+                            onClick = {
+                                splitByState.value =
+                                    if (splitByState.value > 1) splitByState.value - 1
+                                    else 1
+                                totalPerPersonState.value =
+                                    calculateTotalPerPerson(
+                                        totalBillState.value.toDouble(),
+                                        splitByState.value,
+                                        tipPercentage
+                                    )
+
+                            })
+                        Text(
+                            text = "${splitByState.value}",
+                            fontWeight = ExtraBold,
+                            modifier = Modifier
+                                .align(alignment = Alignment.CenterVertically)
+                                .padding(start = 9.dp, end = 9.dp)
+                        )
+                        RoundIconButton(imageVector = Icons.Default.Add,
+                            onClick = {
+                                if (splitByState.value < range.last) {
+                                    splitByState.value = splitByState.value + 1
+                                }
+                                totalPerPersonState.value =
+                                    calculateTotalPerPerson(
+                                        totalBillState.value.toDouble(),
+                                        splitByState.value,
+                                        tipPercentage
+                                    )
+
+                            })
+
+
+                    }
+                }
                 Row(
                     modifier = Modifier.padding(horizontal = 3.dp),
                     horizontalArrangement = Arrangement.End
                 ) {
-                    RoundIconButton(imageVector = Icons.Default.Add,
-                        onClick = { /*TODO*/ })
                     Text(
-                        text = "2",
-                        fontWeight = ExtraBold,
-                        modifier = Modifier
-                            .align(alignment = Alignment.CenterVertically)
-                            .padding(start = 9.dp, end = 9.dp)
+                        text = "Tip ",
+                        modifier = Modifier.align(alignment = Alignment.CenterVertically)
                     )
-                    RoundIconButton(imageVector = Icons.Default.Remove,
-                        onClick = { /*TODO*/ })
+                    Spacer(modifier = Modifier.width(200.dp))
+                    Text(
+                        text = " $ ${tipAmmountState.value}",
+                        modifier = Modifier.align(alignment = Alignment.CenterVertically)
+
+                    )
                 }
-            }
-            Row(
-                modifier = Modifier.padding(horizontal = 3.dp),
-                horizontalArrangement = Arrangement.End
-            ) {
-                Text(
-                    text = "Tip ",
-                    modifier = Modifier.align(alignment = Alignment.CenterVertically)
-                )
-                Spacer(modifier = Modifier.width(200.dp))
-                Text(
-                    text = " $33.00  ",
-                    modifier = Modifier.align(alignment = Alignment.CenterVertically)
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.padding(5.dp)
+                ) {
+                    Text(
+                        text = " $tipPercentage %  ",
 
-                )
-            }
-            Column(
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier=Modifier.padding(5.dp)
-            ) {
-                Text(
-                    text = " 33% ",
+                        )
+                    Spacer(modifier = Modifier.height(14.dp))
+                    Slider(value = sliderPositionState.value,
+                        modifier = Modifier.padding(start = 16.dp, end = 16.dp),
 
+                        onValueChange = {
+                            sliderPositionState.value = it
+                            tipAmmountState.value =
+                                calculateTotalTip(
+                                    totalBillState.value.toDouble(),
+                                    tipPercentage
+                                )
+                            totalPerPersonState.value =
+                                calculateTotalPerPerson(
+                                    totalBillState.value.toDouble(),
+                                    splitByState.value,
+                                    tipPercentage
+                                )
+                        },
+                        steps = 5,
+                        onValueChangeFinished = {
+
+                        }
                     )
-                Spacer(modifier = Modifier.height(14.dp))
-                Slider(value = sliderPositionState.value,
-                    modifier= Modifier.padding(start = 16.dp, end = 16.dp),
+                }
 
-                    onValueChange ={
-
-                    },
-                steps = 5
-                    )
             }
-
-            // }
         }
 
     }
 }
 
-@Preview(showBackground = true)
+
+//@Preview
 @Composable
-fun DefaultPreview() {
-    JetTipAppTheme {
-        MyApp {
-            Text(text = "Hello Again")
+fun TopHeader(totalPerPerson: Double = 134.0) {
+    androidx.compose.material.Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(25.dp)
+            .height(150.dp)
+            .clip(shape = RoundedCornerShape(corner = CornerSize(12.dp))),
+        color = Color(0xFFE9D7F7)
+
+    ) {
+        Column(
+            modifier = Modifier.padding(5.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            val total = "%.2f".format(totalPerPerson)
+            Text(
+                text = "Total Per Person",
+                style = MaterialTheme.typography.h5,
+                fontWeight = Bold
+            )
+            Text(
+                text = "$ $total ",
+                style = MaterialTheme.typography.h4,
+                fontWeight = ExtraBold
+
+            )
 
         }
 
+
     }
+
 }
+
+
